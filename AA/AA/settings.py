7730865301ab9599ebae5afcb3d3f2a1818beb79
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-10-24 22:29:35 +0330

    2aban

--- AA/AA/settings.py
+++ AA/AA/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
-    BASE_DIR / "accounts" / "static"
+    BASE_DIR / "accounts" / "static", BASE_DIR / "blog" / "static"
 ]
+MEDIA_URL= "media/"
+MEDIA_ROOT= BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
